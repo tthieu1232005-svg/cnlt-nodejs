01c73d220c778f655003e354a63ca9448530defb
VanMinhThanhThieu <minh@LAPTOP-RFCV5MQ6.>
2026-03-06 21:34:25 +0700

    Fix routing in index.js

--- TH1/index.js
+++ TH1/index.js
@@ -3,20 +3,28 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     console.log(req.url)
 
-    res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8'
-    })
-
     switch(req.url) {
         case "/":
+            res.writeHead(200, {
+                'Content-Type': 'text/plain; charset=utf-8'
+            })
             res.end("Trang chủ")
             break
+
         case "/about":
+            res.writeHead(200, {
+                'Content-Type': 'text/plain; charset=utf-8'
+            })
             res.end("Trang giới thiệu")
             break
+
         case "/contact":
+            res.writeHead(200, {
+                'Content-Type': 'text/plain; charset=utf-8'
+            })
             res.end("Trang liên hệ")
             break
+
         default:
             res.writeHead(404, {
                 'Content-Type': 'text/plain; charset=utf-8'
@@ -25,4 +33,6 @@ const server = http.createServer((req, res) => {
     }
 })
 
-server.listen(3000)
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000")
+})
