483c6d5f5bd99e066ea8611ac88bd6e3c8685d32
VanMinhThanhThieu <minh@LAPTOP-RFCV5MQ6.>
2026-03-13 08:40:58 +0700

    add TH2

---
+++ TH2/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = new express()
+//Đăng ký thư mục public.
+app.use(express.static('public'))
+app.listen(4000, () => {
+console.log('App listening on port 4000')
+})
